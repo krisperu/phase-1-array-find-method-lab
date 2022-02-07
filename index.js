5b6f126391c22a943e1a989c357f553c14377b4c
Kristina Peru <krissy.germanov@gmail.com>
2022-02-07 00:20:36 -0700

    commit tree ceb4767f995790471b6211d58d423c42a23686ef

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+const superbowlWin = (record) => {
+    let result = record.find((win) => {
+    return win.result ==`W`   
+      })
+      if (result != undefined) {
+        return result.year
+      }
+}
